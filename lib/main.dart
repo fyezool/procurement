39d21064beba581b44713bf0b77e917cfc3261fe
fyezool <fyezool@gmail.com>
2024-07-05 12:39:09 +0800

    changesssss

--- lib/main.dart
+++ lib/main.dart
@@ -11,7 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'FHPE Procurement Database',
+      title: 'FHPESB Procurement Database',
       theme: ThemeData(
         // This is the theme of your application.
         //
