c0111058728188a391a36687f86a46c814865d39
fyezool <fyezool@gmail.com>
2024-07-09 17:36:43 +0800

    finally moved conn to file instead of harcode, the drop down works

--- backend/db.go
+++ backend/db.go
@@ -4,16 +4,35 @@ import (
 	"context"
 	"log"
 	"time"
-
+	"encoding/json"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
 )
 
+type Config struc {
+	MongoDB struct {
+		URI string `json:"uri"`
+	} 	`json:"mongodb"`
+}
+
 var client *mongo.Client
 
 func init() {
-	clientOptions := options.Client().ApplyURI("mongodb://root:example@raspberrypi.diman:27017")
-	var err error
+	//read config file
+	file, err := os.Open("config.json")
+	if err != nil {
+		log.Fatalf("Failed to open config file: %v", err)
+	}
+	defer file.Close
+
+	var config Config
+	decoder := json.NewDecoder(file)
+	err = decoder.Decode(&config)
+	if err != nil {
+		log.Fatalf("Failed to decode config file: %v", err)
+	}
+
+	clientOptions := options.Client().ApplyURI(config.MongoDB.URI)
 	client, err = mongo.Connect(context.Background(), clientOptions)
 	if err != nil {
 		log.Fatalf("Failed to connect to MongoDB: %v", err)
